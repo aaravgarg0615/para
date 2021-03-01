526874ffd3f20a792feae4b43772c792a9d61ae3
aaravgarg0615 <79427022+aaravgarg0615@users.noreply.github.com>
2021-02-28 20:33:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var inputs = []
+function para1()
+{
+    
+    for(var i = 1 ; i<=6 ; i++)
+    {
+        var inputs 
+        inputs.push = document.getElementById("_"+i).value;
+        inputs.join(". ")
+        document.getElementById("showParagraph1").innerHTML = (". ");
+    }
+}
+
+
+function para2()
+for(var i = 1 ; i<=6 ; i++)
+    {
+        var inputs 
+        inputs.push = document.getElementById("2"+i).value;
+        inputs.join(". ")
+        document.getElementById("showParagraph2").innerHTML = (". ");
+    }
+
+
